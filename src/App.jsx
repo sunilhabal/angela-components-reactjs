4c8d06de7bb6094e9be98a74aab4e2966bac82e5
sunilhabal <habalsunil@gmail.com>
2024-07-08 00:06:18 +0530

    List file add in the src folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Heading from './Heading'
+import List from './List'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,11 +11,7 @@ function App() {
   return (
     <>
       <Heading />
-      <ol>
-        <li>Balcon</li>
-        <li>Jamon</li>
-        <li>Noodles</li>
-      </ol>
+      <List />
     </>
   )
 }
---
+++ src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List () {
+  return (
+    <div>
+      <ul>
+        <li>Maggi</li>
+        <li>Chiken</li>
+        <li>Panner thikka</li>
+      </ul>
+    </div>
+  )
+}
+
+export default List;
