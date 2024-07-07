e3d5d32112e1cfdb10b4917918d1e67f1be481d4
sunilhabal <habalsunil@gmail.com>
2024-07-08 00:15:21 +0530

    all componets files add in the Main components folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Heading from './Heading'
-import List from './List'
+import Heading from './components/Heading'
+import List from './components/List'
 
 function App() {
   const [count, setCount] = useState(0)
