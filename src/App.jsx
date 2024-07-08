47304e563446ca170b0f18e049e936209534af45
sunilhabal <habalsunil@gmail.com>
2024-07-08 23:14:07 +0530

    components PI

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Heading from './components/Heading'
 import List from './components/List'
+// import  {Pi,doublePi, triplePi} from './components/Math'
+import * as Pi from './components/Math'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>PI value of:{Pi.default}</h1>
+      <h2>PI value of:{Pi.doublePi()}</h2>
+      <h3>PI value of:{Pi.triplePi()}</h3>
       <Heading />
-      <List />
+      < List />
     </>
   )
 }
---
+++ src/components/Math.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+
+const Pi = 3.142;
+
+function doublePi () {
+   return Pi;
+}
+
+function triplePi () {
+   return Pi;
+}
+
+export default Pi;
+export{doublePi,triplePi};
